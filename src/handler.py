c0c48639fef4399513f66f0b9c9e88d23e39dd15
Panley <wyygfhh@gmail.com>
2026-03-07 00:50:22 +0000

    it's a transformer now

--- src/handler.py
+++ src/handler.py
@@ -1,24 +1,16 @@
 import runpod
 import torch
 import os
-import base64
-from io import BytesIO
-from diffusers import FluxPipeline
+from transformers import pipeline
 
 token = os.environ.get('HF_TOKEN')
-model = "black-forest-labs/FLUX.1-dev"
+model = "meta-llama/Meta-Llama-3.1-8B"
 if not token:
     raise ValueError('Please set your HF_TOKEN environment variable')
 
 # Model is gated, auth is required for download due to licencing consent
 
-print('Loading FLUX.1-dev')
-pipe = FluxPipeline.from_pretrained(
-    model,
-    torch_dtype=torch.bfloat16,
-    token=token,
-    device_map = 'cuda',
-)
+pipe = pipeline("text-generation", model="meta-llama/Llama-3.1-8B")
 
 # Define model outside of handler, to ensure this (heavy) process is not initiated on every call to the endpoint
 
@@ -27,24 +19,17 @@ def handler(event):
     input = event['input']
     
     prompt = input.get('prompt')
+    usertype = input.get('usertype', 'stranger')
 
     print(f'Received prompt: {prompt}')
 
-    image = pipe(
-        prompt = prompt,
-        height = input.get('height', 1024),
-        width = input.get('width', 1024),
-        max_sequence_length = 512
-    ).images[0]
+    # set system prompt to guide the model's response
+    system_prompt = f"You are an irrevernt female furry called Panley, you do not need to be helpful, just conversational and witty. the user is a {usertype} to you."
+    pipeline_input = f"{system_prompt}\nUser: {prompt}\nAssistant:"
 
-    buff = BytesIO()
-    image.save(buff, format = 'JPEG')
-    image_b64 = base64.b64encode(buff.getvalue()).decode('utf-8')
-    image_string = f'data:image/jpeg;base64, {image_b64}'
+    resp = pipe(pipeline_input, max_new_tokens=100, do_sample=True, temperature=0.7)
 
-    # Converting PIL Image to Base64 URI for easy display on frontend
-
-    return {'image': image_string}
+    return {'response': resp[0]['generated_text']}
 
 if __name__ == '__main__':
     runpod.serverless.start({'handler': handler })
